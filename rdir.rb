60daeecba493b54f0e6f788a3b805ed838ac0f9a
Eugene <CromonMS@users.noreply.github.com>
2015-12-11 15:44:56 +0000

    Update rdir.rb
    made into method

--- rdir.rb
+++ rdir.rb
@@ -1,7 +1,9 @@
  # Create and write the contents of current Dir to a text file called dir.txt
  
- File.open('dir.txt', 'a') { |f| f.write(pp Dir["*"]) }
+ # File.open('dir.txt', 'a') { |f| f.write(pp Dir["*"]) }
  
- def dir(filename, dir)
-   File.open('#{filename}.txt', 'a') { |f| f.write(Dir['#{dir}']) }
- end
+# to run:
+# eg: dir('dir', '*') => dir.txt with contents of current directory
+def dir(filename, dir)
+  File.open(filename+'.txt', 'a') { |f| f.write(Dir[dir]) }
+end
